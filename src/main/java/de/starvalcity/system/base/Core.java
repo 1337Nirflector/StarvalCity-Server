8d1635aa4af8d4e03e0b64cfa8c41ed2e61dcd5e
Nirflector <nirflector@gmail.com>
2021-03-01 14:00:01 +0100

    Edited the messages; added a new ArrayList, that includes all Strings (messages) for the onEnable() and onDisable() function

--- src/main/java/de/starvalcity/system/base/Core.java
+++ src/main/java/de/starvalcity/system/base/Core.java
@@ -1,6 +1,7 @@
 package de.starvalcity.system.base;
 
 import de.starvalcity.system.base.commandhandler.CommandHandler;
+import de.starvalcity.system.base.messages.MessagesHandler;
 import org.bukkit.Bukkit;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.PluginManager;
@@ -16,6 +17,13 @@ public final class Core extends JavaPlugin {
     @Override
     public void onEnable() {
         setCoreInstance(this);
+        MessagesHandler.loadErrorMessages();
+        MessagesHandler.loadStaffMessages();
+        MessagesHandler.loadStartupMessages();
+        MessagesHandler.loadSystemMessages();
+
+        printStartupMessages();
+
         commandHandler = new CommandHandler();
         commandHandler.setCommands();
         pluginManager.enablePlugin(this);
@@ -23,6 +31,8 @@ public final class Core extends JavaPlugin {
 
     @Override
     public void onDisable() {
+
+        printShutdownMessages();
 
         pluginManager.disablePlugin(this);
     }
@@ -33,5 +43,19 @@ public final class Core extends JavaPlugin {
 
     private static void setCoreInstance(Core coreInstance) {
         Core.coreInstance = coreInstance;
+    }
+
+    private void printStartupMessages() {
+        consoleCommandSender.sendMessage(MessagesHandler.startupMessages.get(0));
+        consoleCommandSender.sendMessage(MessagesHandler.startupMessages.get(1));
+        consoleCommandSender.sendMessage(MessagesHandler.startupMessages.get(2));
+        consoleCommandSender.sendMessage(MessagesHandler.startupMessages.get(3));
+        consoleCommandSender.sendMessage(MessagesHandler.startupMessages.get(4));
+        consoleCommandSender.sendMessage(MessagesHandler.startupMessages.get(0));
+        consoleCommandSender.sendMessage(MessagesHandler.startupMessages.get(1));
+    }
+
+    private void printShutdownMessages() {
+
     }
 }
--- src/main/java/de/starvalcity/system/base/messages/MessagesHandler.java
+++ src/main/java/de/starvalcity/system/base/messages/MessagesHandler.java
@@ -7,22 +7,34 @@ import java.util.HashMap;
 public class MessagesHandler {
 
     public static HashMap<Integer, String> errorMessages = new HashMap<>();
+    public static HashMap<Integer, String> startupMessages = new HashMap<>();
     public static HashMap<Integer, String> systemMessages = new HashMap<>();
 
-    String basicPrefix = PrefixesHandler.prefixes.get(0);
-    String staffPrefix = PrefixesHandler.prefixes.get(1);
+    public static void loadStartupMessages() {
+        String basicPrefix = PrefixesHandler.prefixes.get(0);
+        String staffPrefix = PrefixesHandler.prefixes.get(1);
+        startupMessages.put(0, basicPrefix + "------------------------------------------------------------");
+        startupMessages.put(1, basicPrefix + "                                                            ");
+        startupMessages.put(2, basicPrefix + "§2Loading StarvalCity-Server! Running on version: §e" + Core.getCoreInstance().getDescription().getVersion());
+        startupMessages.put(3, basicPrefix + "§2API-Version:§e " + Core.getCoreInstance().getDescription().getAPIVersion());
+        startupMessages.put(4, basicPrefix + "§2Authors: §e" + Core.getCoreInstance().getDescription().getAuthors());
+    }
 
-    public void loadErrorMessages() {
+    public static void loadErrorMessages() {
+        String basicPrefix = PrefixesHandler.prefixes.get(0);
+        String staffPrefix = PrefixesHandler.prefixes.get(1);
         errorMessages.put(0, basicPrefix + "§4An internal error has occured. Please watch the console!");
         errorMessages.put(1, basicPrefix + "§4Only players are able to execute this command!");
         errorMessages.put(2, basicPrefix + "§4Too few arguments! Please use §e/plugin help §4in order to get help!");
         errorMessages.put(3, basicPrefix + "§4Invalid argument or command parameters!");
     }
 
-    public void loadStaffMessages() {
+    public static void loadStaffMessages() {
     }
 
-    public void loadSystemMessages() {
+    public static void loadSystemMessages() {
+        String basicPrefix = PrefixesHandler.prefixes.get(0);
+        String staffPrefix = PrefixesHandler.prefixes.get(1);
         systemMessages.put(0, basicPrefix + "§2Current plugin version: §e" + Core.getCoreInstance().getDescription().getVersion());
     }
 }
