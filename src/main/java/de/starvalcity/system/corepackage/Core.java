69cf14fa3e7082a2ab253d63df82a0ece525f659
Nirflector <nirflector@gmail.com>
2021-05-12 00:21:46 +0200

    Added core functions

--- src/main/java/de/starvalcity/system/corepackage/Core.java
+++ src/main/java/de/starvalcity/system/corepackage/Core.java
@@ -1,7 +1,7 @@
 package de.starvalcity.system.corepackage;
 
+import de.starvalcity.system.filespackage.SystemMessagesManager;
 import org.bukkit.plugin.java.JavaPlugin;
-
 import java.util.logging.Logger;
 
 public class Core extends JavaPlugin {
@@ -11,19 +11,18 @@ public class Core extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
-
+        system.info(SystemMessagesManager.startupMessage);
 
     }
 
     @Override
     public void onDisable() {
-
-
+        system.info(SystemMessagesManager.shutdownMessage);
 
     }
 
     public Core getInstance() {
         return instance;
     }
+
 }
--- src/main/java/de/starvalcity/system/filespackage/SystemMessagesManager.java
+++ src/main/java/de/starvalcity/system/filespackage/SystemMessagesManager.java
@@ -2,4 +2,8 @@ package de.starvalcity.system.filespackage;
 
 public class SystemMessagesManager {
 
+    public static String startupMessage = "§2Loading §6StarvalCity§2 system ...";
+
+    public static String shutdownMessage = "§4Disabling §6StarvalCity§2 system ...";
+
 }
